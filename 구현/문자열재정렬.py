a06cc7b299ba6636e647d4f1655a4e0224b1ff27
jsw166098 <jsw166098@gmail.com>
2021-02-10 03:11:54 +0900

    git init

---
+++ 구현/문자열재정렬.py
@@ -0,0 +1,44 @@
+# 문자열 재정렬
+
+# 풀이1: 내풀
+S = input()
+num = 0
+str = []
+
+for s in S:
+    if s.isalpha() == True:
+        str.append(s)
+    else:
+        num += int(s)
+
+str.sort()
+
+for i in str:
+    print(i, end="")
+
+print(num)
+
+## 풀이2
+
+data = input()
+result = []
+value = 0
+
+# 문자를 하나씩 확인하며
+for x in data:
+    # 알파벳인 경우 결과 리스트에 삽입
+    if x.isalpha():
+        result.append(x)
+    # 숫자는 따로 더하기
+    else:
+        value += int(x)
+
+# 알파벳을 오름차순으로 정렬
+result.sort()
+
+# 숫자가 하나라도 존재하는 경우 가장 뒤에 삽입
+if value != 0:
+    result.append(str(value))
+
+# 최종 결과 출력(리스트를 문자열로 변환하여 출력)
+print(''.join(result))
